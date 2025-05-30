62e241b36dd2a44d6081e351c20e355bb70d5a8e
Cellexec <cellexec@outlook.com>
2025-05-30 14:24:27 +0200

    add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+
+// CLI arguments
+#[derive(Parser, Debug)]
+#[command(name = "sopsify")]
+#[command(about = "Replace placeholders in YAML files and encrypt them with GPG", long_about = None)]
+struct Args {
+    // Path to the GPG key (e.g. gpg.asc)
+    #[arg(short, long)]
+    gpg_key: PathBuf,
+
+    // Path to the secrets file (YAML format)
+    #[arg(short, long)]
+    secrets_file: PathBuf,
+
+    // Folder containing secret templates
+    #[arg(short, long)]
+    templates_dir: PathBuf,
+
+    // Output directory (where to write encrypted files)
+    #[arg(short, long)]
+    output_dir: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let args = Args::parse();
+    println!("🔐 Using GPG key: {:?}", args.gpg_key);
+    println!("👀 Loading secrets from: {:?}", args.secrets_file);
+    println!("📂 Scanning templates in: {:?}", args.templates_dir);
+
+    // for now return OK
+    Ok(())
 }
